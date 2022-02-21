e7f57721e90ab3b56fa15eb68ffa856be4240c71
Rodrigo Alfonso <rodri042@gmail.com>
2022-02-21 04:45:14 -0300

    Deploy @ 21/02/2022

---
+++ precache-manifest.579266f5081673e0826348fb02b4b47e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f2a1b87a0d19b603314",
+    "url": "/este-gano/static/css/main.06dbb09a.chunk.css"
+  },
+  {
+    "revision": "1f2a1b87a0d19b603314",
+    "url": "/este-gano/static/js/main.e0aada2d.chunk.js"
+  },
+  {
+    "revision": "8cb15e1cfec23573379a",
+    "url": "/este-gano/static/js/runtime~main.3984c9a9.js"
+  },
+  {
+    "revision": "6bb2372fe7b7eca4994e",
+    "url": "/este-gano/static/js/2.369be766.chunk.js"
+  },
+  {
+    "revision": "c932b411a9a6441490060d72641f3689",
+    "url": "/este-gano/index.html"
+  }
+];
